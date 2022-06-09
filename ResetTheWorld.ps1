735da4c9eb67d7008db4b7e2b73e581aa41e2d06
Paul Mcilreavy <paul@mcilreavy.com>
2022-06-09 21:46:12 +1000

    Add reset the world and run all test scripts

---
+++ ResetTheWorld.ps1
@@ -0,0 +1,13 @@
+Clear-Host;
+Write-Host "Resetting the world..."
+
+Write-Host "Restoring dotnet tools..."
+dotnet tool restore
+
+$sln = "$PSScriptRoot\src\SeedWork.sln"
+
+Write-Host "Cleaning solution..."
+Get-ChildItem -Path $PSScriptRoot\src -Recurse -Directory -Include obj, bin, TestResults | Remove-Item -Recurse
+
+Write-Host "Building API [Debug Mode] ..."
+dotnet build "$sln" --configuration Debug --nologo
---
+++ RunAllTests.ps1
@@ -0,0 +1,26 @@
+Clear-Host;
+
+$apiSln = "$PSScriptRoot\src\SeedWork.sln"
+
+Write-Host "Cleaning TestResult folders..."
+Get-ChildItem -Path "$PSScriptRoot\src\" -Recurse -Directory -Include TestResults | Remove-Item -Recurse
+
+Write-Host "Restoring dotnet tools..."
+dotnet tool restore
+
+Write-Host "Running all tests [Debug Mode] ..."
+$testResultsFolder = "$PSScriptRoot\src\Todo.Tests\TestResults";
+dotnet test "$ApiSln" --configuration Debug --collect:"XPlat Code Coverage" --results-directory "$testResultsFolder" --nologo --verbosity m
+
+Write-Host "Generating coverage report..."
+$recentCoverageFile = Get-ChildItem -Path "$testResultsFolder" -File -Filter coverage.cobertura.xml -Recurse | Sort-Object LastWriteTime | Select-Object -First 1;
+$testCoverageFolder = "$PSScriptRoot\src\Todo.Tests\Coverage";
+$testCoverageHistoryFolder = "$testCoverageFolder\History";
+
+dotnet tool run ReportGenerator "-reports:$($recentCoverageFile.FullName)" "-historydir:$testCoverageHistoryFolder" "-reporttypes:HTMLSummary" "-targetdir:$testCoverageFolder"
+
+Write-Host "Opening coverage report in browser..."
+
+Remove-Item "$testResultsFolder" -Recurse
+Remove-Item "$testCoverageFolder\summary.htm" # for some reason it produces html and html files
+#Invoke-Expression "$testCoverageFolder\summary.html" # uncomment to open in the browser
